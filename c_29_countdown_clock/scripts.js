fb729c3a63752fad85866ced8953aa7723040d28
daseul <7581058@gmail.com>
2023-06-02 02:03:17 +0900

    day29 countdown clock

--- c_29_countdown_clock/scripts.js
+++ c_29_countdown_clock/scripts.js
@@ -1,25 +1,52 @@
 let countdown
 const timerDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
 
 function timer(seconds) {
+  clearInterval(countdown) //초기화 
   const now = Date.now() 
   const then = now + seconds * 1000
   displayTimerLeft(seconds)
+  displayEndTime(then)
 
   countdown = setInterval(() => {
-    const secondsLeft = Math.round(then - Date.now()) / 1000
+    const secondsLeft = Math.round((then - Date.now()) / 1000)
 
     if(secondsLeft < 0) {
       clearInterval(countdown)
       return 
     }
     displayTimerLeft(secondsLeft)
-  }, 1000);
+  }, 1000)
 }
 
 function displayTimerLeft(seconds) {
   const minutes = Math.floor(seconds / 60 )
   const remainderSeconds = seconds % 60
-  const display = `${minutes}:${remainderSeconds}`
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
+  document.title = display
   timerDisplay.textContent = display
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp)
+  const hour = end.getHours()
+  const adjustedHour = hour > 12 ? hour - 12 : hour
+  const minutes = end.getMinutes()
+  endTime.textContent = `Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`
+}
+
+function startTimer() {
+  const second = parseInt(this.dataset.time)
+  timer(second)
+}
+
+buttons.forEach(buttons => buttons.addEventListener('click', startTimer))
+
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault()
+  const mins = this.minutes.value
+  timer(mins * 60)
+  this.reset()
+})
